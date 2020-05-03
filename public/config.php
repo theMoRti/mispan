ab62aebdc2bc0a86998c5bb85f74d4870f3bb709
MoRti <themorti@yahoo.com>
2020-05-03 16:29:39 +0430

    make db connection function to a virable

--- public/config.php
+++ public/config.php
@@ -1,16 +1,5 @@
 <?php
 
-
-$db = "localhost";
-$user = "root";
-$pass = "";
-$server = "mispan";
-
-function config(){
-    
-    $connection = mysqli_connect($db, $user, $pass, $server);
-    return $connection;
-}
-
+$connection = mysqli_connect('localhost', 'root', '', 'mispan');
 
 ?>
