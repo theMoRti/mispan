9db4db18289fda10d9b374d9b71e5aba113268fa
MoRti <themorti@yahoo.com>
2020-05-03 16:41:37 +0430

    edit some sytaxes

--- panel.php
+++ panel.php
@@ -1,8 +1,10 @@
 <?php
 include_once 'public/config.php';
 
+
 $data = $_GET['frm'];
 // echo $data["'name'"];
+
 $nameform = $data["'name'"];
 $emailform = $data["'email'"];
 $textform = $data["'text'"];
@@ -10,7 +12,6 @@ $textform = $data["'text'"];
 $query = "INSERT INTO infotbl (name, email, text, pic) VALUES ('$nameform', '$emailform', '$textform', 'defualt');";
 mysqli_query($connection , $query);
 
-header("location: index.php?submit=ok")
 
 ?>
 <!--Some Examples of table is added here so the backend developer can do the job :D-->
@@ -29,112 +30,6 @@ header("location: index.php?submit=ok")
             <table>
                 <tbody>
                 <!--Main Row-->
-                <tr>
-                <td>ID</td>
-                <td>Name</td>
-                <td>Email</td>
-                <td>Text</td>
-                <td>Image</td>
-                <td>Control</td>
-                </tr>
-                
-                <!--Example Rows-->
-                <tr>
-                    <td>5</td>
-                    <td>Ale</td>
-                    <td><a>Alematin@gmail.com</a></td>
-                    <td><a href="showPage.html" class="text">Text</a></td>
-                    <td><a href="noimg.png" target="_blank" class="img">Image</a></td>
-                    <td>
-                        <ul>
-                            <button>
-                                <li class="fas fa-check"></li>
-                            </button> 
-                        <form action="editPage.html">
-                            <button>
-                                <li class="	fas fa-edit"></li>
-                            </button>
-                        </form>
-                        <form>
-                            <button>
-                                <li class="fas fa-trash"></li>
-                            </button>
-                        </form> 
-                        </ul>
-                    </td>
-                </tr>
-                    <tr>
-                        <td>4</td>
-                        <td>Shampo Parzhak</td>
-                        <td><a>KiramToParzhak@gmail.com</a></td>
-                        <td><a href="showPage.html" class="text">Text</a></td>
-                        <td><a href="noimg.png" target="_blank" class="img">Image</a></td>
-                        <td>
-                            <ul>
-                                <button>
-                                    <li class="fas fa-check"></li>
-                                </button> 
-                            <form action="editPage.html">
-                                <button>
-                                    <li class="	fas fa-edit"></li>
-                                </button>
-                            </form>
-                            <form>
-                                <button>
-                                    <li class="fas fa-trash"></li>
-                                </button>
-                            </form> 
-                            </ul>
-                        </td>
-                    </tr>
-                    <td>3</td>
-                    <td>Morti</td>
-                    <td><a>MortiJaghi@gmail.com</a></td>
-                    <td><a href="showPage.html" class="text">Text</a></td>
-                    <td><a href="noimg.png" target="_blank" class="img">Image</a></td>
-                    <td>
-                        <ul>
-                            <button>
-                                <li class="fas fa-check"></li>
-                            </button> 
-                        <form action="editPage.html">
-                            <button>
-                                <li class="	fas fa-edit"></li>
-                            </button>
-                        </form>
-                        <form>
-                            <button>
-                                <li class="fas fa-trash"></li>
-                            </button>
-                        </form> 
-                        </ul>
-                    </td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Mamad</td>
-                    <td><a>MamadKooni@gmail.com</a></td>
-                    <td><a href="showPage.html" class="text">Text</a></td>
-                    <td><a href="noimg.png" target="_blank" class="img">Image</a></td>
-                    <td>
-                        <ul>
-                            <button>
-                                <li class="fas fa-check"></li>
-                            </button> 
-                        <form action="editPage.html">
-                            <button>
-                                <li class="	fas fa-edit"></li>
-                            </button>
-                        </form>
-                        <form>
-                            <button>
-                                <li class="fas fa-trash"></li>
-                            </button>
-                        </form> 
-                        </ul>
-                    </td>
-                </tr>
-                <tr>
                     <td>1</td>
                     <td>Ali</td>
                     <td><a>Alimatin1010@gmail.com</a></td>
