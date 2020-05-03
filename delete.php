9b6ac73fe876af080f2b79a2b0c17eccf7718826
MoRti <themorti@yahoo.com>
2020-05-03 22:04:47 +0430

    adding delete.php file for delete module in panel

---
+++ delete.php
@@ -0,0 +1,6 @@
+<?php
+$id=$_GET['id'];
+$connection=mysqli_connect('localhost','root','','mispan');
+$query="DELETE FROM infotbl WHERE user_id='$id'";
+mysqli_query($connection,$query);
+header("location:users_list.php?delete=ok");
