74f543cdee5c311e3e9482ee0cea962895664f54
MoRti <themorti@yahoo.com>
2020-05-03 20:32:47 +0430

    delete form actions form index file

--- index.php
+++ index.php
@@ -1,40 +1,3 @@
-<?php
-include_once 'public/config.php';
-
-if (isset($_GET['btn'])){
-
-    // $data will get the frm var information form the form 
-    $data = $_GET['frm'];
-// echo $data["'name'"];
-    $nameform = $data["'name'"];
-    $emailform = $data["'email'"];
-    $textform = $data["'text'"];
-
-    // a file uploader
-    $pic=$_FILES['pic']['name'];
-    mkdir("uploader/".$emailform);
-    
-    // get the format of the file
-    $array=explode(".",$pic);
-    $ext=end($pic);
-
-    // make new name for the uploaded file
-    $newName=rand().".".$ext;
-
-    // put the file into the defined path
-    $form=$_FILES['pic']['tmp_name'];
-    $to="uploader/".$emailform."/".$newName;
-    move_uploaded_file($form,$to);
-
-        // INSERT THE DATA AND UPLODED FILE INTO THE DATABASE 
-        $query = "INSERT INTO infotbl (name, email, text, pic) VALUES ('$nameform', '$emailform', '$textform', '$to');";
-        mysqli_query($connection , $query);
-
-header("Location: index.php");
-
-}
-
-?>
 <html>
     <head><title>Form</title>
         <link rel="stylesheet" type="text/css" href="css/sheet.css">
@@ -49,7 +12,7 @@ header("Location: index.php");
             <!--Title Text-->
             <h1 class="sectionText" style="margin-top: 5px;">Form</h1>
             <!--Inputs-->
-            <form action="" method="get" enctype="multipart/form-data">
+            <form action="proc.php" method="get" enctype="multipart/form-data">
                 <input name="frm['name']" type="text" id='Name' placeholder="Name" onchange="document.getElementById('Name').style.backgroundColor = '#FFFFFF';" ><br/>
                 <input name="frm['email']" type="text" id='Email' placeholder="Email" onchange="document.getElementById('Email').style.backgroundColor = '#FFFFFF';"><br/>
                 <textarea name="frm['text']" id="message" placeholder="Your Message" onchange="document.getElementById('message').style.backgroundColor = '#FFFFFF';"></textarea><br/>
