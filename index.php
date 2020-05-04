4585f0624d2535d85267c9756ebaed2c445aba45
MoRti <themorti@yahoo.com>
2020-05-04 22:44:53 +0430

    some syntax changes on index

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
             <!--Title Text-->
             <h1 class="sectionText" style="margin-top: 5px;">Form</h1>
             <!--Inputs / send data with GET method to proc.php file -->
-            <form action="proc.php" method="get" enctype="multipart/form-data">
+            <form action="public/proc.php" method="get" enctype="multipart/form-data">
                 <input name="frm['name']" type="text" id='Name' placeholder="Name" onchange="document.getElementById('Name').style.backgroundColor = '#FFFFFF';" ><br/>
                 <input name="frm['email']" type="text" id='Email' placeholder="Email" onchange="document.getElementById('Email').style.backgroundColor = '#FFFFFF';"><br/>
                 <textarea name="frm['text']" id="message" placeholder="Your Message" onchange="document.getElementById('message').style.backgroundColor = '#FFFFFF';"></textarea><br/>
