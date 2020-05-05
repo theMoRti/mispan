f098b63ca40b09c394c22ae07d9b65e4fbbcef28
MoRti <themorti@yahoo.com>
2020-05-05 18:56:04 +0430

    fix the double cout in the name of the input in form

--- index.php
+++ index.php
@@ -12,10 +12,10 @@
             <!--Title Text-->
             <h1 class="sectionText" style="margin-top: 5px;">Form</h1>
             <!--Inputs / send data with GET method to proc.php file -->
-            <form action="public/proc.php" method="get" enctype="multipart/form-data">
-                <input name="frm['name']" type="text" id='Name' placeholder="Name" onchange="document.getElementById('Name').style.backgroundColor = '#FFFFFF';" ><br/>
-                <input name="frm['email']" type="text" id='Email' placeholder="Email" onchange="document.getElementById('Email').style.backgroundColor = '#FFFFFF';"><br/>
-                <textarea name="frm['text']" id="message" placeholder="Your Message" onchange="document.getElementById('message').style.backgroundColor = '#FFFFFF';"></textarea><br/>
+            <form action="public/proc.php" method="post" enctype="multipart/form-data">
+                <input name="frm[name]" type="text" id='Name' placeholder="Name" onchange="document.getElementById('Name').style.backgroundColor = '#FFFFFF';" ><br/>
+                <input name="frm[email]" type="text" id='Email' placeholder="Email" onchange="document.getElementById('Email').style.backgroundColor = '#FFFFFF';"><br/>
+                <textarea name="frm[text]" id="message" placeholder="Your Message" onchange="document.getElementById('message').style.backgroundColor = '#FFFFFF';"></textarea><br/>
                 <input name="pic" type="file" class="custom-file-input file">
                 <button name="btn" class="submit" onclick="getValues()">Send</button>
                 
