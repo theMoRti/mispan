661ba4dbd968b037af05a570d8651d9469dc0b33
MoRti <themorti@yahoo.com>
2020-05-17 18:13:51 +0430

    addig login function

--- public/login.php
+++ public/login.php
@@ -1,1 +1,16 @@
 <?php
+// a function for check the data that user submit in form with db datas ( compare the form with db )
+function userlogin($data){
+    $connection = mysqli_connect('localhost', 'root', '', 'mispan');
+    // checking the username
+    $sql = "SELECT * FROM admin_tbl WHERE username='$data[username]'";
+    $row = mysqli_query($connection, $sql);
+    $res = mysqli_fetch_assoc($row);
+    // checking the password
+    if($res['password'] == $data['password']){
+        $_SESSION['user'] = $res['username'];
+        header("location:dashboard.php");
+    } else {
+        header("location:index.php?login=error");
+    }
+}
